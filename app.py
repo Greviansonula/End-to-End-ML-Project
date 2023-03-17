e7fcfe28c30ad4fe2da7f63ad6b61fa055cba713
Grevians Onula <greviansonula@gmail.com>
2023-03-17 20:14:26 +0000

    prediction pipeline complete

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask, request, render_template
 import numpy as np
 import pandas as pd
 
-from sklearn.perprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler
+
+from src.pipeline.predict_pipeline import CustomData, PredictionPipeline
 
 
 application=Flask(__name__)
@@ -20,4 +22,22 @@ def predict_datapoint():
     if request.method == 'GET':
         return render_template('home.html')
     else:
-        pass
+        data = CustomData(
+            gender = request.get('gender'), 
+            race_ethnicity = request.get("ethnicity"), 
+            parental_level_of_education = request.get("parental_level_of_education"), 
+            lunch = request.get("lunch"), 
+            test_preparation_course = request.get("test_preparation_course"), 
+            reading_score = request.get("writing_score"), 
+            writing_score = request.get("reading_score")
+            )
+
+        pred_df = data.get_data_as_data_frame()
+        print(print_df)
+
+        predict_pipeline = PredictionPipeline()
+        results = predict_pipeline.predict(pred_df)
+        return render_template('home.html', results=results[0])
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
